e5c16b941fd71bc07f210569b0f946d3b9aae69a
boynoob <boynoob809@yahoo.com>
2022-05-17 11:37:02 +0530

    dependency management

--- main.go
+++ main.go
@@ -3,8 +3,22 @@ package main
 import "github.com/gin-gonic/gin"
 
 func main() {
+	// declaring the router variable
 	router := gin.Default()
-	router.GET("/", hello) // what does this step do? It is not present in the documentation.
+
+	/*
+	 * HTTP GET request on the root(/) resource and define a function to be called when HTTP request
+	 * hits the root end point. The function sends a JSON response with status code of 200(OK) with
+	 * a body of "message" : "Hello Vishal"
+	 */
+	router.GET("/", hello)
+
+	/*
+	 * Deploy the router on port 8080 using router.Run() method.
+	 * By default HTTP server is listening on port 8080. You can define a different port by adding
+	 * an argument to the Run method. router.Run(":5000"). Note that the port parameter should
+	 * be passed as a string, prepended by a colon punctuation.
+	 */
 	router.Run()
 }
 
